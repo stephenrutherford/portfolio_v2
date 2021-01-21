a43cd91b3cfb4ee3a2ecb7d85b296e3f1bde12f8
Stephen <stephen@rutherford.dev>
2021-01-21 22:56:34 +0000

    Initial commit from Create Next App

---
+++ styles/theme.js
@@ -0,0 +1,11 @@
+import { extendTheme } from "@chakra-ui/react"
+
+const colors = {
+	transparent: "transparent",
+	black: "#000",
+	white: "#fff",
+}
+
+const customTheme = extendTheme({ colors })
+
+export default customTheme
