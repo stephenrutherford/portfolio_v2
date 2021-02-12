3f3172708609388b9f7164c86c6de765c59f54f5
Stephen <stephen@rutherford.dev>
2021-02-12 00:15:57 +0000

    Meta hotfix

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,18 @@ export default class Document extends NextDocument {
 	render() {
 		return (
 			<Html lang='en'>
-				<Head />
+				<Head>
+					<title>Stephen Rutherford</title>
+					<link rel='icon' href='/favicon.ico' />
+					<meta
+						name='description'
+						content='I am a Fraud Anaylst. Check out my projects!'
+						name='keywords'
+						content='Stephen Rutherford, Stephen, Rutherford, Fraud, Risk, Analyst, Investigator, Python, SQL, JSX'
+						name='author'
+						content='Stephen Rutherford'
+					/>
+				</Head>{" "}
 				<body>
 					<ColorModeScript initialColorMode={theme.config.initialColorMode} />
 					<Main />
